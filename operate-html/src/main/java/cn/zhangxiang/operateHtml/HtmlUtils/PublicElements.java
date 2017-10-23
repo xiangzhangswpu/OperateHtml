38edb13f4c1fd9b05396c11a4087eee198ee0809
admin <admin@192.168.30.65>
2017-10-23 15:21:35 +0800

    test first commit

---
+++ operate-html/src/main/java/cn/zhangxiang/operateHtml/HtmlUtils/PublicElements.java
@@ -0,0 +1,17 @@
+package cn.zhangxiang.operateHtml.HtmlUtils;
+
+/**
+* @ClassName: PublicElements 
+* @Description: TODO(生成HTML文档过程中的静态变量) 
+* @author zhangxiang 
+* @date 2017年10月23日 下午3:08:00 
+*
+ */
+public class PublicElements {
+	
+	/**
+	 * 为了生成严格的HTML自定义替换标签
+	 */
+	public static final String ReplaceSign = "staticReplaceSign";
+
+}
